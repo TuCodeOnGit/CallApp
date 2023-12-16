76856782410863fe10f31091cda01b97b65fed8d
Tu <tuminhvo98@gmail.com>
2023-12-17 01:12:59 +0700

    initial

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import http from "http";
+import { Server, Socket } from "socket.io";
+
+const server = http.createServer();
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+  pingInterval: 3000,
+  pingTimeout: 4000,
+});
+io.on("connection", (s: Socket) => {
+    console.log('connect', s.id)
+});
+const port = 4000;
+server
+  .listen(port)
+  .on("error", console.error)
+  .on("listening", () => {
+    console.log(`listening on port ${port}`);
+  });
