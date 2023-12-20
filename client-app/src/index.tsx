7548a92e035895756fbb289f02b6edc08df53c3a
Tú Võ <tuminhvo98@gmail.com>
2023-12-21 04:17:28 +0700

    call success

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,76 +1,169 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"
 
-import { Socket, io } from "socket.io-client";
-import { makeAutoObservable } from "mobx";
-import { observer } from "mobx-react-lite";
-import { useEffect, useRef } from "react";
+import { Socket, io } from "socket.io-client"
+import { makeAutoObservable } from "mobx"
+import { observer } from "mobx-react-lite"
+import { useEffect, useRef } from "react"
 
-import "./index.scss";
+import "./index.scss"
 
-let ws: Socket = io("localhost:4000");
+class State {
+  constructor() {
+    makeAutoObservable(this)
+  }
+  localStream?: MediaStream
+  remoteStreams: { 
+    [userId: string]: 
+    {  
+      stream?: MediaStream,
+      peer?: RTCPeerConnection 
+    } 
+  } = {}
+  camOn = true
+  micOn = true
+  socketId: string
+
+  toggleCam() {
+    this.camOn = !this.camOn
+    if (this.camOn) {
+      startLocalStream(state.camOn, state.micOn)
+    }
+    else {
+      state.localStream?.getVideoTracks()
+        .forEach((track) => track.stop())
+    }
+  }
+  toggleMic() {
+    this.micOn = !this.micOn
+    if (this.micOn) {
+      startLocalStream(state.camOn, state.micOn)
+    } else {
+      state.localStream?.getAudioTracks()
+        .forEach((track) => track.stop())
+    }
+  }
+  setLocalStream(stream: MediaStream) {
+    this.localStream = stream
+  }
+}
+
+const state = new State()
+
+let ws: Socket = io("localhost:4000")
+
+const createPeerConnection = async (id: string, offerSdp?: RTCSessionDescriptionInit) => {
+  console.log('create peer connection for ' + id)
+  const configuration = { 'iceServers': [{ 'urls': ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'] }] }
+  const peer = state.remoteStreams[id].peer = new RTCPeerConnection(configuration)
+  peer.onicecandidate = onPeerIceCandidate
+  peer.ontrack = onPeerTrack
+  state.localStream?.getTracks().forEach(t => peer?.addTrack(t))
+  if (!offerSdp) {
+    const localSdp = await peer.createOffer()
+    peer.setLocalDescription(localSdp)
+    ws.emit('offer', {
+      to: id,
+      offer: localSdp
+    })
+    return
+  }
+  peer.setRemoteDescription(new RTCSessionDescription(offerSdp))
+  const localSdp = await peer.createAnswer()
+  peer.setLocalDescription(localSdp)
+  ws.emit('answer', {
+    to: id,
+    answer: localSdp
+  })
+}
+const onPeerTrack = (e: RTCTrackEvent) => {
+  Object.keys(state.remoteStreams).forEach(key => {
+    state.remoteStreams[key].stream?.addTrack(e.track);
+  })
+}
+
+const joinRoom = () => {
+  ws.emit('joinRoom', { name: "Tu" })
+}
+const onJoinRoomSuccess = (d: { serverSocketId: string, room: string[] }) => {
+  console.log('join room success', d.serverSocketId)
+  state.socketId = d.serverSocketId
+  d.room.forEach(id => {
+    if (!(id in state.remoteStreams) && id != state.socketId) {
+      state.remoteStreams[id] = {
+        stream: new MediaStream()
+      }
+    }
+  });
+  ws.emit('call', {
+    callId: d.serverSocketId
+  })
+}
+const onNewUserJoin = (d: { newUserId: string }) => {
+  state.remoteStreams[d.newUserId] = {
+    stream: new MediaStream()
+  }
+  console.log(Object.keys(state.remoteStreams).length)
+}
+const onOffer = (d: { to: string }) => {
+  createPeerConnection(d.to)
+}
+const onReceiveOffer = (d: { from: string, offer: RTCSessionDescriptionInit }) => {
+  createPeerConnection(d.from, d.offer)
+}
+const onReceiveAnswer = (d: { from: string, answer: RTCSessionDescriptionInit }) => {
+  console.log('receiver answer', d.answer)
+  state.remoteStreams[d.from].peer?.setRemoteDescription(d.answer)
+}
+const onWsIcecandidate = (d: { from: string, icecandidate: RTCIceCandidate | null }) => {
+  console.log('on ws icecandidate')
+  if (d.icecandidate) {
+    state.remoteStreams[d.from].peer?.addIceCandidate(new RTCIceCandidate(d.icecandidate))
+    return
+  }
+}
+
+ws.on('joinRoomSuccess', onJoinRoomSuccess)
+ws.on('newUserJoin', onNewUserJoin)
+ws.on('offer', onOffer)
+ws.on('receiveOffer', onReceiveOffer)
+ws.on('receiveAnswer', onReceiveAnswer)
+ws.on('icecandidate', onWsIcecandidate)
+
 
 const startLocalStream = (camOn: boolean, micOn: boolean) => {
   navigator.mediaDevices
     .getUserMedia({ video: camOn, audio: micOn })
-    .then((stream) => state.setLocalStream(stream));
-};
-
-const joinRoom = () => {
-  ws.emit('joinRoom')
+    .then((stream) => {
+      state.setLocalStream(stream)
+    })
 }
 
-class State {
-  constructor() {
-    makeAutoObservable(this);
-  }
-  localStream?: MediaStream;
-  remoteStreams: MediaStream[];
-  camOn = true;
-  micOn = true;
-
-  toggleCam() {
-    this.camOn = !this.camOn;
-    if (this.camOn) {
-      startLocalStream(state.camOn, state.micOn);
-    }
-    else {
-      state.localStream?.getVideoTracks().forEach((track) => track.stop());
-    }
-  }
-  toggleMic() {
-    this.micOn = !this.micOn;
-    if (this.micOn) {
-      startLocalStream(state.camOn, state.micOn);
-    } else {
-      state.localStream?.getAudioTracks().forEach((track) => track.stop());
-    }
-  }
-  setLocalStream(stream: MediaStream) {
-    this.localStream = stream;
-  }
+const onPeerIceCandidate = (e: RTCPeerConnectionIceEvent) => {
+  ws.emit('icecandidate', e.candidate)
 }
-
-const state = new State();
 
 const Video = ({
   stream,
+  name,
   children,
 }: {
-  stream: MediaStream | undefined;
-  children?: React.ReactNode;
+  name: string,
+  stream: MediaStream | undefined
+  children?: React.ReactNode
 }) => {
-  const ref = useRef<HTMLVideoElement | null>(null);
+  const ref = useRef<HTMLVideoElement | null>(null)
   useEffect(() => {
-    if (!stream || !ref.current) return;
-    ref.current.srcObject = stream;
-  }, [stream]);
+    if (!stream || !ref.current) return
+    ref.current.srcObject = stream
+  }, [stream])
   return (
     <div className="video-wrapper">
+      name: {name}
       <video className="video" ref={ref} playsInline autoPlay muted></video>
       {children}
     </div>
-  );
-};
+  )
+}
 
 const Controls = observer(() => {
   return (
@@ -82,27 +175,29 @@ const Controls = observer(() => {
         {!state.micOn ? "🔉" : "🔇"}
       </button>
     </div>
-  );
-});
+  )
+})
 
 const App = observer(() => {
   useEffect(() => {
-    startLocalStream(true, true);
-  }, []);
+    startLocalStream(true, true)
+  }, [])
   return (
     <div className="app">
       <div className="app-container">
         <h1>One Call</h1>
         <button className="join-button" onClick={() => joinRoom()}>Join Party</button>
         <div className="grid-layout">
-          <Video stream={state.localStream}>
+          <Video name={state.socketId} stream={state.localStream}>
             <Controls />
           </Video>
+          {Object.keys(state.remoteStreams)
+            .map(id => <Video key={id} stream={state.remoteStreams[id].stream} name={id} />)}
         </div>
       </div>
     </div>
-  );
-});
+  )
+})
 
-const root = document.getElementById("root") as HTMLElement;
-createRoot(root).render(<App />);
+const root = document.getElementById("root") as HTMLElement
+createRoot(root).render(<App />)
--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import http from "http";
 import { Server, Socket } from "socket.io";
 
 const server = http.createServer();
+let room: Socket[] = [];
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -10,12 +11,56 @@ const io = new Server(server, {
   pingInterval: 3000,
   pingTimeout: 4000,
 });
+
 io.on("connection", (s: Socket) => {
-    console.log('connect', s.id)
-    s.on("joinRoom", () => {
-      console.log(`socket id ${s.id} `)
+  console.log('connect', s.id)
+  s.on("joinRoom", d => {
+    console.log(`id:${s.id}, name${d.name} join room`)
+    room.push(s);
+    s.emit('joinRoomSuccess', {
+      serverSocketId: s.id,
+      room: room.map(s => s.id)
     })
+    room
+      .filter(o => o.id !== s.id)
+      .forEach(o => o.emit('newUserJoin', { newUserId: s.id }))
+  })
+  s.on('call', (d: { callId: string }) => {
+    if (room.length <= 1) return
+    const caller: Socket = room.find(s => s.id == d.callId) as Socket
+    room.filter(s => s != caller).forEach((s: Socket) => {
+      caller.emit('offer', {
+        to: s.id
+      })
+    })
+  })
+  s.on('offer', (d: {to: string, offer: RTCSessionDescriptionInit }) => {
+    const so = room.find(s => s.id == d.to) as Socket
+    so.emit('receiveOffer', {
+      from: s.id,
+      offer: d.offer
+    })
+  })
+  s.on('answer', (d: { to: string, answer: RTCSessionDescriptionInit }) => {
+    const so = room.find(s => s.id == d.to) as Socket
+    so.emit('receiveAnswer', {
+      from: s.id,
+      answer: d.answer
+    })
+  })
+  s.on('icecandidate', (d: RTCIceCandidate | null) => {
+    room
+      .filter(o => o.id !== s.id)
+      .forEach(o => o.emit('icecandidate', {
+        from: s.id,
+        icecandidate: d
+      }))
+  })
+  s.on('disconnect', () => {
+    room = room.filter(o => s.id !== o.id)
+  })
 });
+
 const port = 4000;
 server
   .listen(port)
